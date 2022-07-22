ca5c78efd8fb37d011de16402cdabcff4cc91859
santhoshsonu <santhosheshwaramma@gmail.com>
2022-07-22 13:49:24 +0530

    Cloud Config Server init commit

---
+++ cloud-config-server/src/main/java/com/sample/cloudconfigserver/CloudConfigServer.java
@@ -0,0 +1,13 @@
+package com.sample.cloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServer.class, args);
+	}
+
+}
---
+++ cloud-config-server/src/test/java/com/sample/cloudconfigserver/CloudConfigServerTests.java
@@ -0,0 +1,13 @@
+package com.sample.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
